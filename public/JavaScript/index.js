e070a0c756db5976750d1cf2e4073c4cfcaec676
Suriya2023 <rajputsuraj221221@gmail.com>
2025-08-28 14:36:21 +0530

    Add dynamic content loading with hideContent toggle and content-area injection for About/Books/Contact pages

--- public/JavaScript/index.js
+++ public/JavaScript/index.js
@@ -1,16 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
     const links = document.querySelectorAll(".load-page");
     const contentArea = document.getElementById("content-area");
+    const hideContent = document.getElementById("hideContent");
 
     links.forEach(link => {
         link.addEventListener("click", function (e) {
-            e.preventDefault(); // Page reload roko
+            e.preventDefault();
             const url = this.getAttribute("href");
 
             fetch(url)
                 .then(res => res.text())
                 .then(data => {
-                    contentArea.innerHTML = data;  
+                    hideContent.style.display = "none";
+contentArea.style.display = "block"
+                    contentArea.innerHTML = data;
                 })
                 .catch(err => {
                     contentArea.innerHTML = "<p class='text-danger'>Error loading page!</p>";
